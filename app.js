f0f43c92e56c6d46fa0940f0e575da954056d025
gabriel <metalmadnes98@gmail.com>
2022-04-24 16:23:19 -0500

    se agrego una nueva aplicacion la cual se llama /launchX

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000 //Hace referencia al localhost:3000
-    // cuando alguien acceda a la url que se selecciono como puerto {localhost:3000} recibira la siguiente respuesta
+
+// cuando alguien acceda a la url que se selecciono como puerto {localhost:3000} recibira la siguiente respuesta
 app.get('/', (req, res) => {
-        // respuesta que se tendra
-        res.send("Hola, que bueno tenerte por aqui");
-    })
-    // inicialiacion de la app
+    // respuesta que se tendra
+    res.send("Hola, que bueno tenerte por aqui");
+})
+
+// Creacion de otra app web
+app.get('/launchX', (req, res) => {
+    // respuesta que se tendra
+    res.send("Hola, Bienvenido a LaunchX");
+})
+
+// inicialiacion de la app
 app.listen(port, () => {
     console.log("server initialized");
 })
