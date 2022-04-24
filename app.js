b73e48b933c86e02e9f6c22f204e4a6b351731c4
gabriel <metalmadnes98@gmail.com>
2022-04-24 16:16:21 -0500

    se realizo el sample para obtener nuestra primer aplicacion de ExpressJS

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000 //Hace referencia al localhost:3000
+    // cuando alguien acceda a la url que se selecciono como puerto {localhost:3000} recibira la siguiente respuesta
+app.get('/', (req, res) => {
+        // respuesta que se tendra
+        res.send("Hola, que bueno tenerte por aqui");
+    })
+    // inicialiacion de la app
+app.listen(port, () => {
+    console.log("server initialized");
+})
