19f029ae6b785f4bab243106816fa7f0da214dd6
gabriel <metalmadnes98@gmail.com>
2022-04-24 16:32:53 -0500

    se corrigio el codigo del ejemplo en el cual se pasan onjetos JS como paraemtros a la respuesta del servidor

--- app.js
+++ app.js
@@ -16,14 +16,16 @@ app.get('/launchX', (req, res) => {
 
 // Creacion de otra app web pasando como parametro el mensaje que se mandara
 app.get('/node', (req, res) => {
-        // respuesta que se tendra
-        res.send("Hola, Bienvenido a LaunchX");
-    })
-    // inicialiacion de la app
-app.listen(port, () => {
     const explorer = { id: 1, name: "ExplorerZ" } //objeto JS
     const explorer1 = { id: 2, name: "ExplorerA" } //objeto JS
-    const explore2 = { id: 3, name: "ExplorerB" } //objeto JS
-    const explore3 = { id: 4, name: "ExplorerC" } //objeto JS
+    const explorer2 = { id: 3, name: "ExplorerB" } //objeto JS
+    const explorer3 = { id: 4, name: "ExplorerC" } //objeto JS
+
+    // respuesta que se tendra
+    res.send([explorer, explorer1, explorer2, explorer3]);
+})
+
+// inicialiacion de la app
+app.listen(port, () => {
     console.log("explorer");
 })
